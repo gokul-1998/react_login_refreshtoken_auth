aeba7979a95f232a372bbac9cde90a6117b48734
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-08-07 21:18:07 +0530

    completed video 5

--- react_login_tut/src/components/Login.jsx
+++ react_login_tut/src/components/Login.jsx
@@ -3,21 +3,19 @@ import React from "react";
 import { useRef, useState, useEffect } from "react";
 
 import useAuth from "../hooks/useAuth";
-import { Link,useNavigate,useLocation  } from "react-router-dom";
-
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 import axios from "../api/axios";
 const LOGIN_URL = "/auth";
 
 const Login = () => {
-  const { setAuth } = useAuth();
-
+  const { setAuth,persist,setPersist } = useAuth();
 
   const navigate = useNavigate();
 
-  const location=useLocation();
+  const location = useLocation();
 
-  const from =location.state?.from?.pathname|| "/"
+  const from = location.state?.from?.pathname || "/";
 
   const userRef = useRef(null);
   const errRef = useRef(null);
@@ -47,71 +45,83 @@ const Login = () => {
       // console.log(JSON.stringify(res));
       const accessToken = res?.data?.accessToken;
       const roles = res?.data?.roles;
-      setAuth({user,pwd,roles,accessToken});
+      setAuth({ user, pwd, roles, accessToken });
 
       setUser("");
       setPwd("");
-      navigate(from,{replace:true});
+      navigate(from, { replace: true });
     } catch (err) {
-      if(!err?.response){
+      if (!err?.response) {
         setErrMsg("No Server Response");
-      } 
-      else if (err?.response?.status === 400) {
+      } else if (err?.response?.status === 400) {
         setErrMsg("Missing Username or Password");
-      } 
-      else if (err?.response?.status === 401) {
-        setErrMsg('Unauthorized');
-      }
-      else {
+      } else if (err?.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
         setErrMsg("Login Failed");
       }
       errRef.current.focus();
-
-      
     }
   };
 
+  const togglePersist = () => {
+    setPersist(!persist);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('persist',persist);
+  }, [persist]);
+
+
   return (
-     
-        <section>
-          <p
-            ref={errRef}
-            className={errMsg ? "errmsg" : "offscreen"}
-            aria-live="assertive"
-          >
-            {errMsg}
-          </p>
-          <h1>Sign In</h1>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              ref={userRef}
-              //   this is used , when we load the page, the cursor will be in the username field
-              autoComplete="off"
-              onChange={(e) => setUser(e.target.value)}
-              value={user}
-              required
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={pwd}
-              onChange={(e) => setPwd(e.target.value)}
-              required
-            />
-            <button type="submit">Sign In </button>
-          </form>
-          <p>
-            Need an Account? <br />
-            <span className="link">
-              <a href="/register">Sign Up</a>
-            </span>
-          </p>
-        </section>
-      )}
-  
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          //   this is used , when we load the page, the cursor will be in the username field
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={pwd}
+          onChange={(e) => setPwd(e.target.value)}
+          required
+        />
+        <button type="submit">Sign In </button>
+        <div className="persistCheck">
+          <input 
+          type="checkbox"
+          id="persist"
+          onChange={togglePersist}
+          checked={persist}
+          />
+          <label htmlFor="persist">Trust this device</label>
+        </div>
+      </form>
+      <p>
+        Need an Account? <br />
+        <span className="link">
+          <a href="/register">Sign Up</a>
+        </span>
+      </p>
+    </section>
+  );
+};
 
 export default Login;
--- react_login_tut/src/components/PersistLogin.jsx
+++ react_login_tut/src/components/PersistLogin.jsx
@@ -1,47 +1,40 @@
 import { Outlet } from "react-router-dom";
 
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import useRefreshToken  from "../hooks/useRefreshToken";
+import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
 
 const PersistLogin = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const refresh = useRefreshToken();
-    const { auth } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth, persist } = useAuth();
 
-    useEffect(() => {
-        const verifyRefreshToken=async()=>{
-            try{
-                await refresh();
-            }
-            catch(err){
-                console.error(err);
-            }
-            finally{
-                setIsLoading(false);
-            }
-        }
+  useEffect(() => {
+    let isMounted=true;
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
 
-        !auth?.accessToken ? verifyRefreshToken():setIsLoading(false);
-    },[])
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
 
-    useEffect(()=>{
+    return ()=>isMounted=false;
+  }, []);
 
-        console.log(`isLoading: ${isLoading}`);
-        console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
-    },[isLoading])
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
+  }, [isLoading]);
 
-
-    return (
-        <>
-        {isLoading ? 
-        <p>Loading...</p>
-        :<Outlet/>
-        }
-        </>
-    )
-
-}
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  );
+};
 
 export default PersistLogin;
