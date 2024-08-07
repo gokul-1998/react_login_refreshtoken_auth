7d446621184adfc89d9dc3dd4a4ad13495cb66d9
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-08-07 12:45:55 +0530

    video 3 completed

--- react_login_tut/src/App.jsx
+++ react_login_tut/src/App.jsx
@@ -1,41 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Register from './components/Register'
-import Login from './components/Login'
-import { Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout'
-import LinkPage from './components/LinkPage'
-import Unauthorized from './components/Unauthorized'
-import RequireAuth from './components/RequireAuth'
-import Home from './components/Home'
-import Editor from './components/Editor'
-import Admin from './components/Admin'
-import Lounge from './components/Lounge'
-import Missing from './components/Missing'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import LinkPage from "./components/LinkPage";
+import Unauthorized from "./components/Unauthorized";
+import RequireAuth from "./components/RequireAuth";
+import Home from "./components/Home";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
 
 const ROLES = {
-  'User': 2001,
-  'Editor': 1984,
-  'Admin': 5150
-}
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Routes>
-      <Route path="/" element={<Layout/>}>
-      {/* public routes */}
-      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="linkpage" element={<LinkPage />} />
         <Route path="unauthorized" element={<Unauthorized />} />
 
-
-      {/* we want to protect these routes */}
-      <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
           <Route path="/" element={<Home />} />
           {/* we can have multiple paths too */}
         </Route>
@@ -44,22 +43,21 @@ function App() {
           <Route path="editor" element={<Editor />} />
         </Route>
 
-
         <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
           <Route path="admin" element={<Admin />} />
         </Route>
 
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+        <Route
+          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
           <Route path="lounge" element={<Lounge />} />
         </Route>
 
         {/* catch all */}
         <Route path="*" element={<Missing />} />
-
-
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- react_login_tut/src/components/Users.jsx
+++ react_login_tut/src/components/Users.jsx
@@ -1,51 +1,49 @@
-import { useState,useEffect } from "react"
-
+import { useState, useEffect } from "react";
 import axios from "../api/axios";
 import useRefreshToken from "../hooks/useRefreshToken";
 
 const Users = () => {
-    const [users,setUsers]=useState([]);
+    const [users, setUsers] = useState([]);
+    const refresh = useRefreshToken();
 
-    useEffect(()=>{
-        let isMounted=true;
+    useEffect(() => {
+        let isMounted = true;
         const controller = new AbortController();
-        const refresh=useRefreshToken();
 
-        const getUsers= async()=>{
-            try{
-                const response= await axios.get("/users",{signal:controller.signal});
+        const getUsers = async () => {
+            try {
+                const response = await axios.get("/users", { signal: controller.signal });
                 console.log(response.data);
-                if(isMounted){
+                if (isMounted) {
                     setUsers(response.data);
                 }
-            }catch(err){
+            } catch (err) {
                 console.log(err);
             }
-        }
+        };
 
         getUsers();
-        return()=>{
-            isMounted=false;
+
+        return () => {
+            isMounted = false;
             controller.abort();
-        }
+        };
+    }, [refresh]);
 
-
-    },[])
-  return (
-    <article>
-        <h2>USers List</h2>
-        {
-            users?.length?(
+    return (
+        <article>
+            <h2>Users List</h2>
+            {users?.length ? (
                 <ul>
-                    {users.map((user,i)=><li key={i}>
-                        {user?.username}</li>)}
+                    {users.map((user, i) => <li key={i}>{user?.username}</li>)}
                 </ul>
-            ):<p>No users to display</p>
-        }
-        <button onClick={()=>refresh()}>Refresh</button>
-        <br />
-    </article>
-  )
-}
+            ) : (
+                <p>No users to display</p>
+            )}
+            <button onClick={refresh}>Refresh</button>
+            <br />
+        </article>
+    );
+};
 
-export default Users
+export default Users;
