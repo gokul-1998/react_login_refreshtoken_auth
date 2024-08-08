5c5aa9f402b9e7a0d83758d2692a11010b1bcb05
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-08-08 12:13:12 +0530

    login auth bug fixed

--- redux_auth/src/features/auth/Login.jsx
+++ redux_auth/src/features/auth/Login.jsx
@@ -1,64 +1,56 @@
-import { useRef,useState,useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import { useDispatch } from "react-redux"
-import { useLoginMutation } from "./authApiSlice"
-import { setCredentials } from "./authSlice"
+import { useRef, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useDispatch } from 'react-redux'
+import { setCredentials } from './authSlice'
+import { useLoginMutation } from './authApiSlice'
 
 const Login = () => {
-    const userRef=useRef(null)
-    const errRef=useRef(null)
-    const [user,setUser]=useState("")
-    const [pwd,setPwd]=useState("")
-    const [errMsg,setErrMsg]=useState("")
-    const navigate=useNavigate()
+    const userRef = useRef()
+    const errRef = useRef()
+    const [user, setUser] = useState('')
+    const [pwd, setPwd] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    const navigate = useNavigate()
 
-    const [login, { isLoading, isError, error }] = useLoginMutation()
-    const dispatch=useDispatch()
+    const [login, { isLoading }] = useLoginMutation()
+    const dispatch = useDispatch()
 
-
-    useEffect(()=>{
+    useEffect(() => {
         userRef.current.focus()
-    },[])
+    }, [])
 
-    useEffect(()=>{
-        setErrMsg("")
-    }
-    ,[user,pwd])
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd])
 
-
-    const handleSubmit=async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        try{
-            const userData=await login({user,pwd}).unwrap()
-            dispatch(setCredentials({...userData,user}))
-            setUser("")
-            setPwd("")
-            navigate("/welcome")
 
-        } catch(err){
-            if(!err?.response){
-                console.error(err)
-                setErrMsg("No server response")
-            }else if(err?.response?.status===400){
-                setErrMsg("Missing Username or Password")
-                }
-            else if (err?.response?.status===401){
-                setErrMsg("unauthorized")
+        try {
+            const userData = await login({ user, pwd }).unwrap()
+            dispatch(setCredentials({ ...userData, user }))
+            setUser('')
+            setPwd('')
+            navigate('/welcome')
+        } catch (err) {
+            if (!err?.originalStatus) {
+                // isLoading: true until timeout occurs
+                setErrMsg('No Server Response');
+            } else if (err.originalStatus === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.originalStatus === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
             }
-            else{
-                setErrMsg("Login failed")
-                }
-            errRef.current.focus()
+            errRef.current.focus();
         }
     }
 
-    const handleUserInput=(e)=>{
-        setUser(e.target.value)
-    }
-    const handlePwdInput=(e)=>{
-        setPwd(e.target.value)
-    }
+    const handleUserInput = (e) => setUser(e.target.value)
 
+    const handlePwdInput = (e) => setPwd(e.target.value)
 
     const content = isLoading ? <h1>Loading...</h1> : (
         <section className="login">
@@ -93,5 +85,4 @@ const Login = () => {
 
     return content
 }
-
 export default Login
